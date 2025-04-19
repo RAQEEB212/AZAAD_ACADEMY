bbc1dbd7da980a5da0aa75a4b0bbfaeb121ddad4
RAQEEB212 <saneelmemon45@gmail.com>
2025-04-19 16:03:39 +0500

    job card customize

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -11,10 +11,10 @@ export default function JobCard({id, title, org, deadline }: JobCardProps) {
         <div className="bg-white p-4 shadow rounded-md border hover:shadow-md transition">
             <h3 className="text-lg font-semibold text-blue-700">{title}</h3>
             <p className="text-sm text-gray-600">Organization: {org}</p>
-            <p className="text-sm text-red-500">Deadline: {deadline}</p>
+
 <div className="flex flex-wrap  justify-between">
     <div>
-
+        <p className="text-sm font-bold mt-4 text-red-500">Deadline: {deadline}</p>
     </div>
     <div>
         <Link
