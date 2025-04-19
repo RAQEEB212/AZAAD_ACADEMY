d8ac8293040e0f15d219b6da6e28893c48de2a86
RAQEEB212 <saneelmemon45@gmail.com>
2025-04-19 13:19:24 +0500

    jobs page added

---
+++ src/data /jobs.ts
@@ -0,0 +1,20 @@
+export const jobs = [
+    {
+        id: 1,
+        title: 'Punjab Police Inspector',
+        org: 'PPSC',
+        deadline: '30 Apr 2025',
+    },
+    {
+        id: 2,
+        title: 'Junior Clerk',
+        org: 'SPSC',
+        deadline: '05 May 2025',
+    },
+    {
+        id: 3,
+        title: 'Lecturer - Computer Science',
+        org: 'FPSC',
+        deadline: '10 May 2025',
+    },
+];
