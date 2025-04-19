1b54d5269cc23ccab3ad3144cdc8816f369e0e24
RAQEEB212 <saneelmemon45@gmail.com>
2025-04-19 15:21:17 +0500

    ASI job posted

--- src/data /jobs.ts
+++ src/data /jobs.ts
@@ -2,7 +2,7 @@ export const jobs = [
     {
         id: 1,
         title: 'Recruitment To The Post of Assistant Sub-Inspector (BPS-11) - (2025)',
-        org: 'PPSC',
+        org: 'SINDH POLICE',
         deadline: '12-05-2025',
     },
 
