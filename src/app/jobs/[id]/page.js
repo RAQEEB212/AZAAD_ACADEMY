26b8cbec4dfd1b8b0db44e7db28516dd236c9320
RAQEEB212 <saneelmemon45@gmail.com>
2025-04-20 22:13:52 +0500

    share functionalty improved

--- src/app/jobs/[id]/page.js
+++ src/app/jobs/[id]/page.js
@@ -58,7 +58,7 @@ export default function JobDetailPage({ params }) {
                     href={job.Apply}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                    className="px-4 py-2 bg-blue-700 text-white rounded hover:bg-blue-700"
                 >
                     Apply Now
                 </a>
