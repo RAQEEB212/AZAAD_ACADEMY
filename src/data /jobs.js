a4a8616ba1ed0bc522cfb011113251cd3985ce2d
RAQEEB212 <saneelmemon45@gmail.com>
2025-04-21 14:11:17 +0500

    Recruitment To The Post of Civil Judge‑cum‑Magistrate (BS-17) - (2025)

--- src/data /jobs.js
+++ src/data /jobs.js
@@ -8,4 +8,14 @@ export const jobs = [
         deadline: '12-05-2025',
         Advertisment: 'https://res.cloudinary.com/df3ok5t8p/image/upload/v1745076420/AZAAD%20ACADEMY/Advertisements/2025/lbqodf4lrgsvjomfskos.jpg',
     },
+    {
+        id: 2,
+        title: 'Recruitment To The Post of Civil Judge‑cum‑Magistrate (BS-17) - (2025)',
+        description:
+            'Applications are invited on the "Prescribed Form" from eligible candidates domiciled in Punjab for appointment to vacant temporary (likely to become permanent) posts of Civil Judge‑cum‑Magistrate (BS-17) under the Punjab Judicial Service Rules, 1994.',
+        Apply: 'https://www.lhc.gov.pk/jobs',
+        org: 'Examination Wing, Lahore High Court, Lahore',
+        deadline: '05-05-2025',
+        Advertisment: 'https://res.cloudinary.com/df3ok5t8p/image/upload/v1745226614/AZAAD%20ACADEMY/Advertisements/2025/v1ndk9tfb6und9iao7na.jpg', // add your advertisement URL here later
+    }
 ];
